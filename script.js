4da10e9bd0c98f2bd0a9f6d65d3b3fc075fe7c31
r333z <233511778+r333z@users.noreply.github.com>
2025-12-06 18:46:37 -0800

    Deleted and recreated functions are revisiting learning material Both functions successfully log choices allowed or given

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 // Function which decides the computer's choice
-function getComputerChoice() {
-  const randomNumber = Math.floor(Math.random() * 3);
+// Give computer choices to select from
+// Make those choices random
+// Return choices
+function getComputerChoice(comChoice) {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randNumber = Math.floor(Math.random() * 3) 
+        return choices[randNumber];
+    }
+// Function that allows the user to input a choice
+// Prompt the user with a window with choices
+// Return choices to the console
+function getHumanChoice() {
+    let userInput =  prompt("rock, paper, or scissors.. choose wisely");
+    const choices = ['rock', 'paper', 'scissors'];
+    let selectedChoice = null;
+        if (userInput) {
+            const normalInput = userInput.trim().toLowerCase();
+        for (const choice of choices) {
+            if(choice.toLowerCase() === normalInput) {
+                selectedChoice = choice;
+                break;
 
-  if (randomNumber === 0) {
-    return "Rock";
-  } else if (randomNumber === 1) {
-    return "Paper";
-  } else (randomNumber === 2) 
-    return "Scissors";
-  }
+            }
+        }
+        }
+        if (selectedChoice) {
+            console.log(`You chose ${selectedChoice}`);
+        }
+        else {
+            console.log("Invalid selection, or no selection made.");
+        }
+}
 
-const computerChoice = getComputerChoice();
-console.log(computerChoice);
+getHumanChoice();

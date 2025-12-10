8e6fdd79acbdee0012740d228df574f09f6dc557
r333z <233511778+r333z@users.noreply.github.com>
2025-12-09 21:21:12 -0800

    Recreated getHumanChoice with simple if else loop Created round logic with playRound function where getHumanChoice and getComputerChoice functions are called, looped checked until a condition is met

--- script.js
+++ script.js
@@ -1,35 +1,50 @@
-// Function which decides the computer's choice
-// Give computer choices to select from
-// Make those choices random
-// Return choices
-function getComputerChoice(comChoice) {
-    const choices = ['rock', 'paper', 'scissors'];
-    const randNumber = Math.floor(Math.random() * 3) 
-        return choices[randNumber];
-    }
-// Function that allows the user to input a choice
-// Prompt the user with a window with choices
-// Return choices to the console
+// Global variables for round wins start
+let humanScore = 0;
+let compScore = 0;
+
+// Computer choice
+// Computer selects a one of the available choices
+// Choice is the randomized and returned
+function getComputerChoice() {
+  const choices = ['rock', 'paper', 'scissors'];
+  const randIndex = Math.floor(Math.random() * 3);
+  return choices[randIndex];
+} 
+
+// Player inputs a selection
+// Prompt loops until player chooses correctly.
 function getHumanChoice() {
-    let userInput =  prompt("rock, paper, or scissors.. choose wisely");
-    const choices = ['rock', 'paper', 'scissors'];
-    let selectedChoice = null;
-        if (userInput) {
-            const normalInput = userInput.trim().toLowerCase();
-        for (const choice of choices) {
-            if(choice.toLowerCase() === normalInput) {
-                selectedChoice = choice;
-                break;
+  const choice = ['rock', 'paper', 'scissors'];
+  let selectedChoice;
 
-            }
-        }
-        }
-        if (selectedChoice) {
-            console.log(`You chose ${selectedChoice}`);
-        }
-        else {
-            console.log("Invalid selection, or no selection made.");
-        }
+  while(true) {
+    let selectedChoice = prompt("Rock, paper, or scissors.. Choose wisely")
+  if (choice.includes(selectedChoice.toLowerCase())) {
+    return selectedChoice.toLowerCase();
+  } else {
+    alert("Invalid choice. Please enter rock, paper, or scissors")
+  }
+  }
 }
+// Round logic for game
+// playRound function calls functions getHumanChoice and getComputerChoice
+// and then loops them until one of the conditions is met
+function playRound(humanChoice, compChoice) {
+  humanChoice = getHumanChoice().toLowerCase();
+  compChoice = getComputerChoice().toLowerCase();
 
-getHumanChoice();
+  if (humanChoice === compChoice) {
+  return "It's a tie";
+  }
+  else if ((humanChoice === 'rock' && compChoice === 'scissors' ) ||
+     (humanChoice === 'paper' && compChoice === 'rock') ||
+     (humanChoice === 'scissors' && compChoice === 'paper'))
+  {
+    return `You win. ${humanChoice} beats ${compChoice}.`;
+  }
+    else {
+      return `You lose. ${compChoice} beats ${humanChoice}.`;
+    }
+    }
+
+console.log(playRound());
